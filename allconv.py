d642d15302c482504062b5f935d8d1e5bd1b014f
Abhishek Kumar <vibrantabhi19@gmail.com>
2017-03-07 11:31:13 +0530

    Data Augmentation added

--- allconv.py
+++ allconv.py
@@ -112,10 +112,16 @@ X_train /= 255
 X_test /= 255
 
 datagen = ImageDataGenerator(
-        rescale=1./255,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True) 
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
+        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=False) 
 
 datagen.fit(X_train)
 filepath="weights.hdf5"
